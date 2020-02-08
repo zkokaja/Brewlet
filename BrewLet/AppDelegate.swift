b66211571d03fb01fd72508466d2fda33b2687e9
Zaid Kokaja <zkokaja@gmail.com>
2020-02-08 14:45:50 -0500

    Initial Commit

---
+++ BrewLet/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BrewLet
+//
+//  Created by zzada on 2/8/20.
+//  Copyright © 2020 zzada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
