2c85f17a72fafbccab8dcbdbc143ec7aa6455227
Zach Berger <zach@zberger.com>
2020-11-29 13:43:10 -0800

    Set default brew location based on platform.
    * If a brew location is not set, Brewlet will select the default location based on the current running platform.
    * Refactor preferences to have radio buttons for Apple Silicon, Intel, and custom locations

--- Brewlet/PreferencesController.swift
+++ Brewlet/PreferencesController.swift
@@ -24,8 +24,17 @@ class PreferencesController: NSWindowController {
     @IBOutlet weak var autoUpgrade: NSButton!
     @IBOutlet weak var dontNotifyAvailable: NSButton!
     @IBOutlet weak var brewPath: NSTextField!
-    
+    @IBOutlet weak var intel: NSButton!
+    @IBOutlet weak var appleSilicon: NSButton!
+    @IBOutlet weak var custom: NSButton!
+
     var delegate: PreferencesDelegate?
+    
+    enum HomebrewPath: String {
+        case appleSilicon = "/opt/homebrew/bin/brew"
+        case intel = "/usr/local/bin/brew"
+        case custom = ""
+    }
     
     override func windowDidLoad() {
         super.windowDidLoad()
@@ -60,8 +69,20 @@ class PreferencesController: NSWindowController {
         autoUpgrade.state = defaults.bool(forKey: "autoUpgrade") ? .on : .off
         dontNotifyAvailable.state = defaults.bool(forKey: "dontNotify") ? .on : .off
         
-        let currentBrewPath = defaults.string(forKey: "brewPath") ?? "/usr/local/bin/brew"
-        brewPath.stringValue = currentBrewPath
+        #if arch(arm64)
+        let currentBrewPath = defaults.string(forKey: "brewPath") ?? HomebrewPath.appleSilicon.rawValue
+        #elseif arch(x86_64)
+        let currentBrewPath = defaults.string(forKey: "brewPath") ?? HomebrewPath.intel.rawValue
+        #endif
+        switch currentBrewPath {
+        case HomebrewPath.intel.rawValue:
+            intelSelected(nil)
+        case HomebrewPath.appleSilicon.rawValue:
+            appleSiliconSelected(nil)
+        default:
+            custom.state = .on
+            brewPath.stringValue = currentBrewPath
+        }
     }
     
     @IBAction func includeDependenciesPressed(_ sender: NSButton) {
@@ -94,6 +115,34 @@ class PreferencesController: NSWindowController {
         }
         
         delegate?.updateIntervalChanged(newInterval: seconds)
+    }
+    
+    @IBAction func appleSiliconSelected(_ sender: Any?) {
+        appleSilicon.state = .on
+        intel.state = .off
+        custom.state = .off
+        brewPath.isEnabled = false
+        let path = HomebrewPath.appleSilicon.rawValue
+        brewPath.stringValue = path
+        delegate?.brewPathChanged(newPath: path)
+    }
+    
+    @IBAction func intelSelected(_ sender: Any?) {
+        intel.state = .on
+        appleSilicon.state = .off
+        custom.state = .off
+        brewPath.isEnabled = false
+        let path = HomebrewPath.intel.rawValue
+        brewPath.stringValue = path
+        delegate?.brewPathChanged(newPath: path)
+    }
+    
+    @IBAction func customSelected(_ sender: Any) {
+        custom.state = .on
+        appleSilicon.state = .off
+        intel.state = .off
+        brewPath.isEnabled = true
+        delegate?.brewPathChanged(newPath: brewPath.stringValue)
     }
     
     @IBAction func brewPathChanged(_ sender: NSTextField) {
